dfb4d9b5dc7617f1194294897d37a82067867fa5
Arturo Mora <luartmgr@gmail.com>
2021-10-23 19:16:26 -0600

    [Add] Send JSON with Curl. Receive  JSON with Ulfius

--- client/src/main.c
+++ client/src/main.c
@@ -1,22 +1,123 @@
+/**
+ * @file main.c
+ * @brief Scheduler of threads client program. Sends payloads to be processed to the server.
+ * @author Arturo Mora
+ *
+ * @date 10/21/2021
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <curl/curl.h>
 #include "infrastructure/services/hello-world-service.h"
 
+#define FLAG_FILE "-f"
+#define FLAG_STRG "-t"
+#define DEBUG 1
+
+/**
+ * Read a text file.
+ * @author Arturo Mora
+ * @param filename Name of the file.
+ * @param fcontent Pointer to the content result.
+ * @date 10/18/2021
+ */
+int read_file(char *filename, char *fcontent);
+
+/**
+ * Send HTTP Post with JSON.
+ * @author Arturo Mora
+ * @date 10/22/2021
+ */
+int send_post(char *fcontent);
+
 int main(int argc, char *argv[])
 {
-    char *url = getenv("REST_URL");
+    {
+
+        char *fcontent = NULL;
+
+        if (argc < 3) // no arguments were passed
+        {
+            printf("[!] Usage is %s [ -f file_name |-t {\"Ta\":t, file:name_1},...,{Ta:t, file:name_n}]] ]  \n", argv[0]);
+            return -1;
+        }
+
+        if (strcmp(FLAG_FILE, argv[1]) == 0) // file mode
+        {
+            read_file(argv[2], fcontent);
+        }
+        else if (strcmp(FLAG_STRG, argv[1]) == 0) // text mode
+        {
+            fcontent = argv[2];
+        }
+        else
+        {
+            printf("[!] Usage is %s [ -f name |-t [{Ta:t, file name_1}]] ]  \n", argv[0]);
+            return -1;
+        }
+        if (DEBUG)
+        {
+            printf("[D] Content:\n%s\n", fcontent);
+        }
+
+        // CURL Hello
+        send_post(fcontent);
+        return EXIT_SUCCESS;
+    }
+}
+
+int read_file(char *filename, char *fcontent)
+{
+    FILE *fp = fopen(filename, "r");
+    int fsize = 0;
+    if (fp == NULL)
+    {
+        printf("[E] Cannot open %s \n", filename);
+        return EXIT_FAILURE;
+    }
+    fseek(fp, 0, SEEK_END);
+    fsize = ftell(fp);
+    rewind(fp);
+    fcontent = (char *)malloc(sizeof(char) * fsize);
+    fread(fcontent, 1, fsize, fp);
+    if (fclose(fp) == EOF)
+    {
+        printf("[E] Failed to close %s \n", filename);
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
+int send_post(char *fcontent)
+{
+    char *url = "http://127.0.0.1:6969/server/"; // TODO = getenv("REST_URL");
     CURL *curl;
     CURLcode res;
-
     curl_global_init(CURL_GLOBAL_ALL);
-
     curl = curl_easy_init();
     if (curl)
     {
-        get_hello_world(curl, url);
-        post_hello_world(curl, url);
+        struct curl_slist *chunk = NULL;
+        chunk = curl_slist_append(chunk, "Content-Type: application/json");
+
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, fcontent);
+        res = curl_easy_perform(curl);
+        if (DEBUG)
+        {
+            printf("[D] Response Code:\n%i", res);
+        }
+        if (res != CURLE_OK)
+        {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        }
+        /* always cleanup */
+        curl_easy_cleanup(curl);
+        curl_slist_free_all(chunk);
     }
-    return 0;
+    curl_global_cleanup();
+    return EXIT_SUCCESS;
 }
--- server/src/main.c
+++ server/src/main.c
@@ -1,14 +1,39 @@
-#include <stdio.h>
-#include <ulfius.h>
-#include "api/routes/routes.h"
+/**
+ * @file main.c
+ * @brief Scheduler of threads server program. Receives payloads and Schedules threads
+ * @author Arturo Mora
+ *
+ * @date 10/21/2021
+ */
 
-#define PORT 8080
+#include <stdio.h>
+#include <yder.h>
+#include <jansson.h>
+#include <ulfius.h>
+
+#define PORT 6969
+#define PREFIX "/server"
+#define OBJ_MAIN "payload"
+#define OBJ_PATA "Ta"
+#define OBJ_PAFN "filename"
+
+/**
+ * Get a JSON Request.
+ * @author Arturo Mora
+ * @param request HTTP request.
+ * @param response HTTP response.
+ * @param userData Information attached to body.
+ * @date 10/22/2021
+ */
+int callback_post(const struct _u_request *request, struct _u_response *response, void *userData);
 
 int main(void)
 {
 
   struct _u_instance instance;
+  json_int_t nb_sheep = 0;
 
+  y_init_logs("Server", Y_LOG_MODE_CONSOLE, Y_LOG_LEVEL_DEBUG, NULL, "[+] Server Starting...");
   // Initialize instance with the port number
   if (ulfius_init_instance(&instance, PORT, NULL, NULL) != U_OK)
   {
@@ -17,23 +42,50 @@ int main(void)
   }
 
   // Endpoint list declaration
-  add_routes(&instance);
+  ulfius_add_endpoint_by_val(&instance, "POST", PREFIX, NULL, 1, &callback_post, &nb_sheep);
 
   // Start the framework
   if (ulfius_start_framework(&instance) == U_OK)
   {
-    printf("Start framework on port %d\n", instance.port);
-    while(1);
+    y_init_logs("Server", Y_LOG_MODE_CONSOLE, Y_LOG_LEVEL_DEBUG, NULL, "[+] Server Listening...");
+
+    // Wait for the user to press <enter> on the console to quit the application
+    getchar();
   }
   else
   {
     fprintf(stderr, "Error starting framework\n");
     fprintf(stderr, "Error code: %i\n", ulfius_start_framework(&instance));
   }
-  printf("End framework\n");
+  y_init_logs("Server", Y_LOG_MODE_CONSOLE, Y_LOG_LEVEL_DEBUG, NULL, "[-] Server Stopping...");
 
   ulfius_stop_framework(&instance);
   ulfius_clean_instance(&instance);
 
   return 0;
+}
+
+int callback_post(const struct _u_request *request, struct _u_response *response, void *userData)
+{
+  json_t *json_nb_payload = ulfius_get_json_body_request(request, NULL), *json_body = NULL, *obj = NULL;
+  json_int_t *nb_sheep = userData;
+
+  size_t index = 0;
+  json_t *value, *data_fn, *data_ta;
+
+  if (json_nb_payload != NULL)
+  {
+    *nb_sheep = json_object_size(obj);
+    obj = json_object_get(json_nb_payload, OBJ_MAIN);
+    json_array_foreach(obj, index, value)
+    {
+      value = json_array_get(obj, index);         // Get element of array
+      data_ta = json_object_get(value, OBJ_PATA); // Get Key OBJ_PATA
+      data_fn = json_object_get(value, OBJ_PAFN); // Get Key OBJ_PAFN
+      y_log_message(Y_LOG_LEVEL_DEBUG, "\t%s \t%s \t%s", "Index", "Ta", "Filename");
+      y_log_message(Y_LOG_LEVEL_DEBUG, "\t%i \t%i \t%s", index, json_integer_value(data_ta), json_string_value(data_fn));
+    }
+  }
+  json_decref(json_nb_payload);
+  return U_CALLBACK_CONTINUE;
 }
