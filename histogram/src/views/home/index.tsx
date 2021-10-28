869324b58b4bbe046207bf57501b8019ec2c474e
Abstractize <gababarca@hotmail.com>
2021-10-28 11:01:52 -0600

    [fix] routes

--- histogram/src/views/home/index.tsx
+++ histogram/src/views/home/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Jumbotron } from 'reactstrap';
 import { Props } from './props';
-import Histogram from '../../components/histogram';
+import Histogram from '../../components/histogram'
 
 export class Home extends React.PureComponent<Props> {
     public render() {
