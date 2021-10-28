bde7448a8212bf3a9d3f51ba628effe5706f7c00
Arturo Mora <luartmgr@gmail.com>
2021-10-28 09:05:58 -0600

    [Fix] Added GTK for Cmake

--- server/src/managers/view-manager.h
+++ server/src/managers/view-manager.h
@@ -11,17 +11,17 @@
 #define BAR_PROG_3 "bar_prog_3"
 #define BAR_PROG_4 "bar_prog_4"
 #define BAR_PROG_5 "bar_prog_5"
-    
+
+void start_gui(GtkBuilder *builder, GtkProgressBar *bar_prog_1, GtkProgressBar *bar_prog_2, GtkProgressBar *bar_prog_3, GtkProgressBar *bar_prog_4, GtkProgressBar *bar_prog_5, GtkFlowBox *fbx_queue, GtkFlowBox *fbx_time);
+
+void test_color(GtkFlowBox *flowbox);
+
+static GtkWidget *
+color_swatch_new(const gchar *color);
+
+static gboolean
+draw_color(GtkWidget *drawingarea,
+           cairo_t *cr,
+           const char *color_name);
+
 #endif
-// void start_gui(GtkBuilder *builder, GtkProgressBar *bar_prog_1,  GtkProgressBar *bar_prog_2,  GtkProgressBar *bar_prog_3,  GtkProgressBar *bar_prog_4,  GtkProgressBar *bar_prog_5, GtkFlowBox *fbx_queue, GtkFlowBox *fbx_time);
-
-// void test_color(GtkFlowBox *flowbox);
-
-// static GtkWidget *
-// color_swatch_new(const gchar *color);
-
-// static gboolean
-// draw_color(GtkWidget *drawingarea,
-//            cairo_t *cr,
-//            const char *color_name);
-
