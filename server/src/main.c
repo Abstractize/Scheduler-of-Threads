ad484ed1c2fd9a1aecac8e2086baace46235a85d
idleStardust <5619808+idleStardust@users.noreply.github.com>
2021-10-29 01:16:10 -0600

    Feature/#12 timeline (#31)
    * [ADD] Button Signal
    
    * [add] Button, Label and ProgressBar on Sync
    
    * Delete builderx.ui

--- server/src/main.c
+++ server/src/main.c
@@ -13,8 +13,11 @@
 #include <gtk/gtk.h>
 #include "./api/routes/routes.h"
 #include "./managers/scheduler-manager.h"
+#include "./managers/action-manager.h"
+#include "./managers/expo-manager.h"
 #include "./managers/view-manager.h"
 #include "./managers/listener-manager.h"
+
 
 #define PORT 5000
 #define QUANTUM 1
@@ -45,30 +48,16 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		GtkBuilder *builder;
-		GObject *window;
-		GtkProgressBar *bar_prog_1, *bar_prog_2, *bar_prog_3, *bar_prog_4, *bar_prog_5;
-		GtkFlowBox *fbx_queue, *fbx_time;
-		GError *error = NULL;
 
 		pthread_t t0;
 		if (pthread_create(&t0, NULL, &start_listener, NULL) == -1)
 		{
 			printf("[!] Cannot create thread\n");
 		}
+
 		gtk_init(&argc, &argv);
 		/* Construct a GtkBuilder instance and load our UI description */
-		// gtk_progress_bar_set_fraction(bar_prog_3, 0.9);
-		builder = gtk_builder_new_from_file("builderx.ui");
-		bar_prog_1 = GTK_PROGRESS_BAR(gtk_builder_get_object(builder, BAR_PROG_1));
-		bar_prog_2 = GTK_PROGRESS_BAR(gtk_builder_get_object(builder, BAR_PROG_2));
-		bar_prog_3 = GTK_PROGRESS_BAR(gtk_builder_get_object(builder, BAR_PROG_3));
-		bar_prog_4 = GTK_PROGRESS_BAR(gtk_builder_get_object(builder, BAR_PROG_4));
-		bar_prog_5 = GTK_PROGRESS_BAR(gtk_builder_get_object(builder, BAR_PROG_5));
-		fbx_queue = GTK_FLOW_BOX(gtk_builder_get_object(builder, FBX_QUEUE));
-		fbx_time = GTK_FLOW_BOX(gtk_builder_get_object(builder, FBX_TIME));
-		test_color(fbx_queue);
-		test_color(fbx_time);
+		start_gui();
 	}
 
 	// Run GTK
---
+++ server/src/managers/expo-manager.h
@@ -0,0 +1,18 @@
+#include <gtk/gtk.h>
+
+#ifndef EXPO_MANAGER_HEADER_H
+#define EXPO_MANAGER_HEADER_H
+
+const int get_count();
+
+const float get_progress_1();
+
+const float get_progress_2();
+
+const float get_progress_3();
+
+const float get_progress_4();
+
+const float get_progress_5();
+
+#endif // EXPO_MANAGER_HEADER_H
